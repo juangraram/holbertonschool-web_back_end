0cc0a530c4805a7a316ea4b69c7de12af1ccfaf0
juangraram <juangraram@zohomail.com>
2021-07-19 16:16:22 -0500

    Add import math

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """function floor which takes a float n"""
+import math
 
 
 def floor(n: float) -> int:
     """returns the floor of the float number"""
-    return int(n)
+    return math.floor(n)
