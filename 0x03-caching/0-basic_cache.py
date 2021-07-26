a4f4387ab5f3d9f71e3af6cab204face9f90336b
juangraram <juangraram@zohomail.com>
2021-07-26 10:35:06 -0500

    Basic dictionary

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        """Put a item"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return a value linked to a key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
