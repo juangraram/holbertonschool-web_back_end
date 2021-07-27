4fadf21ee87d1f5742405fefc86ed884db93def1
juangraram <juangraram@zohomail.com>
2021-07-26 20:13:26 -0500

    LIFO Caching

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""self descriptive code"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Class LIFO inherits from base caching"""
+
+    def __init__(self):
+        super().__init__()
+        self.key_indexes = []
+
+    def put(self, key, item):
+        """Method put"""
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key in self.cache_data:
+                    del self.cache_data[key]
+                    self.key_indexes.remove(key)
+                else:
+                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
+                    item_discarded = self.key_indexes.pop(self.MAX_ITEMS - 1)
+                    print("DISCARD:", item_discarded)
+
+            self.cache_data[key] = item
+            self.key_indexes.append(key)
+
+    def get(self, key):
+        """Method get"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
