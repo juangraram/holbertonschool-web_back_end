d89e1848556595d41b297ab713d8b40408cb066d
juangraram <juangraram@zohomail.com>
2021-07-26 19:53:19 -0500

    Change method put

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -4,11 +4,14 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """BaseCaching (Parent class)"""
+    def __init__(self):
+        super().__init__()
 
     def put(self, key, item):
         """Put a item"""
         if key and item:
-            self.cache_data[key] = item
+            self.cache_data.update({key: item})
 
     def get(self, key):
         """Return a value linked to a key"""
