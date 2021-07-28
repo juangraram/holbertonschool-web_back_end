b42b977bb933564e9a6b8e6692ba3fc35c1badc8
juangraram <juangraram@zohomail.com>
2021-07-28 09:59:51 -0500

    Pycodestyle line 6

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Method return a tuple of size two containing a
         start index and an end index"""
