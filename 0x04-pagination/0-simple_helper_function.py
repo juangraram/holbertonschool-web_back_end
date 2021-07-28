33406e14983683742bf27412ce998f6b0fedd2e3
juangraram <juangraram@zohomail.com>
2021-07-28 09:57:48 -0500

    function named index_range

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Simple helper function pagination"""
+
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Method return a tuple of size two containing a
+        start index and an end index"""
+    start = ((page - 1) * page_size)
+    end = page * page_size
+    return start, end
